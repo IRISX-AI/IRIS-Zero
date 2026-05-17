1087afd72868bf25a3b99e49ba4c2d0d4a575317
Harsh <pandeygautam039@gmail.com>
2026-05-18 04:43:43 +0530

    feat: initialize App component with IRIS Zero branding screen

--- src/client/App.tsx
+++ src/client/App.tsx
@@ -2,7 +2,7 @@ const App = () => {
   return (
     <>
       <div className="w-full h-screen bg-black flex justify-center items-center">
-        <h1 className="font-bold text-3xl text-center">IRIS Zero</h1>
+        <h1 className="font-bold text-8xl text-center text-white">IRIS Zero</h1>
       </div>
     </>
   );
