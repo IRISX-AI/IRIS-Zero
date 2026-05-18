41589e731e03cbb8225fa94a32c73ccaa43223b1
Harsh <pandeygautam039@gmail.com>
2026-05-18 12:06:05 +0530

    feat: initialize express server with vite-express integration

--- src/server/main.ts
+++ src/server/main.ts
@@ -4,6 +4,7 @@ import ViteExpress from "vite-express";
 const app = express();
 const port = Number(process.env.PORT) || 4894;
 
-ViteExpress.listen(app, port, () =>
-  console.log(`Server is running on http://localhost:${port}`),
-);
+ViteExpress.listen(app, port, () => {
+  console.clear();
+  console.log(`Server is running on http://localhost:${port}`);
+});
