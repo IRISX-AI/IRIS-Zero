9d3818f042a231c50aabadc2f67da2df7e2cc807
Harsh <pandeygautam039@gmail.com>
2026-05-30 19:56:54 +0530

    update App.tsx

--- src/client/App.tsx
+++ src/client/App.tsx
@@ -1,9 +1,9 @@
+import IrisZero from "./IrisZero";
+
 const App = () => {
   return (
     <>
-      <div className="w-full h-screen bg-black flex justify-center items-center">
-        <h1 className="font-bold text-8xl text-center text-white">IRIS Zero</h1>
-      </div>
+      <IrisZero />
     </>
   );
 };
