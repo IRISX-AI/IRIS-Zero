b1689a6a6770d1a4605cbde81980b7bd9e94d4ca
Harsh <pandeygautam039@gmail.com>
2026-06-01 18:48:00 +0530

    update voice.route.ts

--- src/server/router/voice.route.ts
+++ src/server/router/voice.route.ts
@@ -1,5 +1,9 @@
 import { Router } from "express";
+import { VoiceStart, VoiceStop } from "../controllers/voice.controller.js";
 
 const VoiceRouter = Router();
+
+VoiceRouter.post("/start", VoiceStart);
+VoiceRouter.post("/stop", VoiceStop);
 
 export default VoiceRouter;
